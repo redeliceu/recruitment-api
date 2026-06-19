2558606242caccc1a359739a84ff5a4d6a1380fd
HenriqueCardoso-Liceu <tecnologia3@liceubrasil.com.br>
2026-06-19 12:01:14 -0300

    Update SendWeeklyVacanciesReport.php

--- app/Console/Commands/SendWeeklyVacanciesReport.php
+++ app/Console/Commands/SendWeeklyVacanciesReport.php
@@ -6,6 +6,7 @@ use App\Mail\WeeklyVacanciesMail;
 use App\Services\OpenVacanciesService;
 use App\Services\VacancyReportService;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use Throwable;
 
@@ -21,6 +22,7 @@ class SendWeeklyVacanciesReport extends Command
     ) {
         try {
 
+            Log::info('Iniciando envio do relatório semanal.');
 
             $report = $builder->build($api->get());
 
@@ -29,14 +31,22 @@ class SendWeeklyVacanciesReport extends Command
             unset($report['recipients']);
 
             foreach ($recipients as $recipient) {
+                Log::info("Enviando para {$recipient['email']}");
 
-                Mail::to($recipient['email'])
-                    ->queue(
-                        new WeeklyVacanciesMail($report)
-                    );
+                try {
+                    Mail::to($recipient['email'])
+                        ->queue(new WeeklyVacanciesMail($report));
+
+                    $this->info("✔ {$recipient['email']}");
+                } catch (\Throwable $e) {
+                    report($e);
+
+                    $this->error("Erro ao enviar para {$recipient['email']}: {$e->getMessage()}");
+                }
             }
 
             $this->info('Relatório enviado.');
+            Log::info('Relatório semanal enviado com sucesso.');
 
             return self::SUCCESS;
         } catch (Throwable $e) {
